69a6d9830e75d69f70a4f0ff8f71f83ae7f36540
Alexey Golodnykh <tin234@mail.ru>
2023-07-26 17:53:54 +0300

    Fix for uploading Crome driver

--- UI-tests/conftest.py
+++ UI-tests/conftest.py
@@ -6,7 +6,9 @@ import pytest
 
 @pytest.fixture()
 def driver():
-    driver_service = Service(ChromeDriverManager().install())
+    driver_service = Service(ChromeDriverManager(
+        url = "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json", 
+        driver_version = "115.0.5790.3").install())
     driver = webdriver.Chrome(service=driver_service)
     driver.maximize_window()
     yield driver
