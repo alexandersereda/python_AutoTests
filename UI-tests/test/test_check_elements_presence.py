116629721f030333a2ae037968b1dde27c09757b
Alexander Sereda <alexander.sereda@gmail.com>
2023-08-03 19:16:07 +0100

    add test_check_elements_presence2.py

--- UI-tests/test/test_check_elements_presence.py
+++ UI-tests/test/test_check_elements_presence.py
@@ -2,17 +2,28 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-@pytest.fixture
-def driver():
-    # Инициализация драйвера браузера перед каждым тестом
-    driver = webdriver.Chrome()  # Предполагается, что у вас установлен Chrome WebDriver
-    yield driver
-    # Закрытие браузера после каждого теста
-    driver.quit()
+# @pytest.fixture
+# def driver():
+#     # Инициализация драйвера браузера перед каждым тестом
+#     driver = webdriver.Chrome() 
+#     yield driver
+#     # Закрытие браузера после каждого теста
+#     driver.quit()
+
+# def test_check_elements_presence(driver):
+#     # Открываем страницу для теста
+#     driver.get("https://dostaevsky.ru/")
+
+def read_config():
+    with open("conf.yaml", "r") as file:
+        return yaml.safe_load(file)
 
 def test_check_elements_presence(driver):
-    # Открываем страницу для теста (замените URL на свой)
-    driver.get("https://dostaevsky.ru/")
+    config = read_config()
+    baseurl = config["baseurl"]["BASEURL"]
+    
+    # Открываем страницу для теста
+    driver.get(baseurl)
 
     # Находим элементы с помощью XPath и проверяем их наличие
     element1_present = len(driver.find_elements(By.XPATH, "//a[@class='main-nav__link main-nav__link_promo' and @href='/promo']")) > 0
@@ -22,3 +33,22 @@ def test_check_elements_presence(driver):
     # Проверяем результаты теста
     assert element1_present, "Element 1 not found on the page."
     assert element2_present, "Element 2 not found on the page."
+    assert element3_present, "Element 3 not found on the page."
+
+    # Подсчитываем количество элементов <li class="main-nav__item">
+    main_nav_list = driver.find_element(By.XPATH, "//ul[@class='main-nav__list']")
+    li_elements = main_nav_list.find_elements(By.XPATH, ".//li[@class='main-nav__item']")
+    li_count = len(li_elements)
+
+    print(f"Number of <li class='main-nav__item'> elements: {li_count}")
+    
+    # Проверяем наличие элемента "Избранное", если пользователь авторизован
+    try:
+        favorite_element_present = len(driver.find_elements(By.XPATH, "//div[@class='main-nav__fav main-nav__link main-nav__link_fav' and @href='/favorites']")) > 0
+    except:
+        favorite_element_present = False
+
+    if favorite_element_present:
+        print("Element 'Избранное' is present.")
+    else:
+        print("Element 'Избранное' is not present.")
---
+++ UI-tests/test/test_check_elements_presence2.py
@@ -0,0 +1,50 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture(scope="session")
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+def test_check_element1_presence(driver, baseurl):
+    driver.get(baseurl)
+    element1_present = len(driver.find_elements(By.XPATH, "//a[@class='main-nav__link main-nav__link_promo' and @href='/promo']")) > 0
+    assert element1_present, "Элемент 'Акции' не найден на странице."
+
+def test_check_element2_presence(driver, baseurl):
+    driver.get(baseurl)
+    element2_present = len(driver.find_elements(By.XPATH, "//a[@class='title-secondary']")) > 0
+    assert element2_present, "Элемент 'Популярное' не найден на странице."
+
+def test_check_element3_presence(driver, baseurl):
+    driver.get(baseurl)
+    element3_present = len(driver.find_elements(By.XPATH, "//a[@class='footer-link__link' and @href='/license-agreement']")) > 0
+    assert element3_present, "Элемент 'Лицензионное соглашение' не найден на странице."
+
+def test_count_li_elements(driver, baseurl):
+    driver.get(baseurl)
+    main_nav_list = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//ul[@class='main-nav__list']"))
+    )
+    li_elements = main_nav_list.find_elements(By.XPATH, ".//li[@class='main-nav__item']")
+    li_count = len(li_elements)
+    assert li_count > 0, "No <li class='main-nav__item'> elements found on the page."
+    print(f"Number of <li class='main-nav__item'> elements: {li_count}")
+
+def test_check_favorite_element(driver, baseurl):
+    driver.get(baseurl)
+    try:
+        favorite_element_present = len(driver.find_elements(By.XPATH, "//div[@class='main-nav__fav main-nav__link main-nav__link_fav' and @href='/favorites']")) > 0
+    except:
+        favorite_element_present = False
+
+    if favorite_element_present:
+        print("Element 'Избранное' is present.")
+    else:
+        print("Element 'Избранное' is not present.")
